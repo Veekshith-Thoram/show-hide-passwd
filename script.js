6dd94ff5161c453fbe4a2cc659bf63e577dc0ba9
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-02-03 05:22:45 -0600

    animation,keyframes, settimeout()

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 console.log("Hey Sadhv Thalli")
 const togglePasswordButton = document.getElementById("togglePassword");
 const passwordInput = document.getElementById("password");
+const spinner = document.getElementById("spinner");
 
 togglePasswordButton.addEventListener("click", () => {
-    if(passwordInput.type === "password"){
-        passwordInput.type = "text";
-        togglePasswordButton.textContent = "Hide Password";
-        togglePasswordButton.classList.remove("show-password");
-        togglePasswordButton.classList.add("hide-password");
-    } else {
-        passwordInput.type = "password";
-        togglePasswordButton.textContent = "Show Password";
-        togglePasswordButton.classList.remove("hide-password");
-        togglePasswordButton.classList.add("show-password")
-    }
+    spinner.style.display = "block";
+    togglePasswordButton.textContent = "loading...";
+    setTimeout(()=> {
+        if(passwordInput.type === "password"){
+            passwordInput.type = "text";
+            togglePasswordButton.textContent = "Hide Password";
+            togglePasswordButton.classList.remove("show-password");
+            togglePasswordButton.classList.add("hide-password");
+        } else {
+            passwordInput.type = "password";
+            togglePasswordButton.textContent = "Show Password";
+            togglePasswordButton.classList.remove("hide-password");
+            togglePasswordButton.classList.add("show-password")
+        }
+        spinner.style.display = "none";
+    }, 1000)
+    
 })
