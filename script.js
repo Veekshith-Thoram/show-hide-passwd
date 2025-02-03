06b6f41c73b8e9f0cdcf4d29c28fcbe5cc56bf22
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-02-03 03:26:59 -0600

    initial html and javascript wiring and code execution

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hey Sadhv Thalli")
+const togglePasswordButton = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePasswordButton.addEventListener("click", () => {
+    if(passwordInput.type === "password"){
+        passwordInput.type = "text";
+        togglePasswordButton.textContent = "Hide Password";
+        togglePasswordButton.classList.remove("show-password");
+        togglePasswordButton.classList.add("hide-password");
+    } else {
+        passwordInput.type = "password";
+        togglePasswordButton.textContent = "Show Password";
+        togglePasswordButton.classList.remove("hide-password");
+        togglePasswordButton.classList.add("show-password")
+    }
+})
